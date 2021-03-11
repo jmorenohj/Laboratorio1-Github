a821c2875d2aff5c295c6d573a3002efdccc1515
SebasPQ <sebas01pq@gmail.com>
2021-03-10 22:58:49 -0500

    Añadida función que opera 3 números enteros

--- src/FunctionXD.java
+++ src/FunctionXD.java
@@ -5,7 +5,12 @@ public class FunctionXD {
         else
             return fib(n - 1) + fib(n - 2);
     }
+    public static int operation(int num1, int num2, int num3){
+        return num1*num2 + num3;
+    }
     public static void main(String[] args) {
+        System.out.println("The result of the operation is: " + operation(7,4,9));
+        System.out.println("The result of the operation is: " + operation(5,5,6));
         System.out.println("The 0th fibonacci number is: " + fib(0));
         System.out.println("The 7th fibonacci number is: " + fib(7));
         System.out.println("The 12th fibonacci number is: " + fib(12));
